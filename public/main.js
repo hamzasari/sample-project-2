07cc6dd7f57409e6ebe20d1a22a1d76ccdad1d56
hamzasari <38205549+hamzasari@users.noreply.github.com>
2021-09-01 16:35:26 +0300

    Global interval is stopped if no events in queue, ignoring type message older than 20 seconds logic is modified.

--- public/main.js
+++ public/main.js
@@ -5,50 +5,42 @@ import { addMessage, animateGift, isPossiblyAnimatingGift, isAnimatingGiftUI } f
 
 const api = new APIWrapper();
 
+const defaultIntervalTimeout = 500;
+let globalInterval = null;
 let eventQueue = [];
 
 api.setEventHandler((events) => {
 
   if (events.length > 0) {
-    // I used sortEvents method here to prioritize animated gifts over other types and after prioritizing I added them to message queue.
+    // I used sortEvents method here to prioritize animated gifts over other types and after prioritizing I added them to the event queue.
     // Because, I understand from the document that every received event block has to evaluate on its own.
-    // If animated gifts are always wanted to be first elements in message queue then we have to sort eventQueue in every 500 milliseconds,
+    // If animated gifts are always wanted to be first elements in the event queue then we have to sort eventQueue if new events come,
     // but it is not efficient and I didn't get it from the document.
     eventQueue.push(...sortEvents(events));
 
     // Handle duplicates
     removeDuplicateEvents();
+
+    if (!globalInterval) {
+      // UI update interval is 500 milliseconds
+      globalInterval = setInterval(() => {
+        checkEventQueueAndMakeNecessaryDomUpdate();
+      }, defaultIntervalTimeout);
+    }
   }
 
-})
+});
 
 // NOTE: UI helper methods from `dom_updates` are already imported above.
 
 /**
- * Removes duplicate events from event queue
+ * Removes duplicate events by id from event queue
  */
 const removeDuplicateEvents = () => {
   eventQueue = Array.from(new Set(eventQueue.map(item => item.id)))
     .map(id => {
       return eventQueue.find(item => item.id === id)
     });
-};
-
-/**
- * Removes events with type "Message" older than 20 seconds
- */
-const removeEventsWithTypeMessageOlderThan20Seconds = () => {
-  eventQueue = eventQueue.filter((event) => {
-    if (event.type === API_EVENT_TYPE.MESSAGE) {
-      const differenceInMilliseconds = new Date().getTime() - new Date(event.timestamp).getTime();
-      const differenceInSeconds = differenceInMilliseconds / 1000;
-      if (differenceInSeconds <= 20) {
-        return event;
-      }
-    } else {
-      return event;
-    }
-  });
 };
 
 /**
@@ -85,18 +77,49 @@ const getFirstNotAnimatedGiftEventIndex = () => {
 };
 
 /**
+ * Calculates time difference in seconds according to now
+ * @param timestamp
+ * @returns {number}
+ */
+const calculateTimeDifferenceInSecondsAccordingToNow = (timestamp) => {
+  const differenceInMilliseconds = new Date().getTime() - new Date(timestamp).getTime();
+  return differenceInMilliseconds / 1000;
+};
+
+/**
+ * Ignores first event with type message older than 20 seconds and controls for other elements in order
+ */
+const ignoreFirstEventWithTypeMessageOlderThan20Seconds = () => {
+  // work only if current element type is message
+  while (eventQueue[0].type === API_EVENT_TYPE.MESSAGE) {
+    const differenceInSeconds = calculateTimeDifferenceInSecondsAccordingToNow(eventQueue[0].timestamp);
+    if (differenceInSeconds > 20) {
+      // if current element is message and it is older than 20 seconds than ignore it
+      eventQueue.shift();
+    } else {
+      // otherwise break the loop
+      break;
+    }
+  }
+}
+
+/**
  * Checks event queue and makes necessary dom updates if event queue has any element and removes that element from event queue
  */
 const checkEventQueueAndMakeNecessaryDomUpdate = () => {
+  // Check length of event queue, if it is empty stop global interval
   if (eventQueue.length <= 0) {
+    clearInterval(globalInterval);
+    globalInterval = null;
     return;
   }
 
-  removeEventsWithTypeMessageOlderThan20Seconds();
+  // ignore first item if type is message and it is older than 20 seconds
+  ignoreFirstEventWithTypeMessageOlderThan20Seconds();
 
   let currentEvent = null;
 
-  if (eventQueue[0].type === API_EVENT_TYPE.ANIMATED_GIFT) {
+  if (eventQueue[0].type === API_EVENT_TYPE.ANIMATED_GIFT) { // if current event is an animated gift
     if (!isAnimatingGiftUI() && !isPossiblyAnimatingGift()) { // if there is no animating gift that is currently playing
       currentEvent = eventQueue.shift();
       addMessage(currentEvent);
@@ -110,13 +133,8 @@ const checkEventQueueAndMakeNecessaryDomUpdate = () => {
         addMessage(currentEvent);
       }
     }
-  } else {
+  } else { // if current event is not an animated gift
     currentEvent = eventQueue.shift();
     addMessage(currentEvent);
   }
 };
-
-// UI update interval is 500 milliseconds
-setInterval(() => {
-  checkEventQueueAndMakeNecessaryDomUpdate();
-}, 500);
